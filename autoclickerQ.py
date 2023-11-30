f4361b9200a3d1756b996d9c440cd2ce083e1f5a
Gene Ishchuk <eugen.istschuk@gmail.com>
2023-11-30 01:47:37 +0100

    Update autoclickerQ.py

--- autoclickerQ.py
+++ autoclickerQ.py
@@ -4,8 +4,8 @@ import pyautogui
 import time
 from tqdm import tqdm
 
-for num in tqdm(range(10)):
-    time.sleep(random.randint(1, 20))
+for num in tqdm(range(30)):
+    time.sleep(random.randint(1, 10))
     pyautogui.click(305,740)
     time.sleep(random.randint(3, 5))
     pyautogui.click(684,324)
